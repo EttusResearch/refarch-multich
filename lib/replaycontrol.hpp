3a804b9be806a6ad6f530a13555efe212e8ce22b
Dylan Baros <dylan.baros@ni.com>
2021-09-08 10:59:42 -0500

    Write to mem seems to work.

--- lib/replaycontrol.hpp
+++ lib/replaycontrol.hpp
@@ -12,6 +12,7 @@ class ReplayControl{
         static int singleTXLoopbackMultithread(GraphSettings& graphSettings, SignalSettings& signalSettings, DeviceSettings& deviceSettings);
         static int allTX(GraphSettings& graphSettings, SignalSettings& signalSettings, DeviceSettings& deviceSettings);
         static int allRX(GraphSettings& graphSettings, SignalSettings& signalSettings, DeviceSettings& deviceSettings);
+        static int singleTXLoopbackMultithread16(GraphSettings& graphSettings, SignalSettings& signalSettings, DeviceSettings& deviceSettings);
 
 
 
