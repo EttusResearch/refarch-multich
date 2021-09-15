8e509d4f59b8a618e1472c7569c314b1021ab45e
jovian <jovian.wysocki@ni.com>
2021-09-15 14:51:29 -0500

    Added a bit more proof of concept

--- lib/graphassembly.hpp
+++ lib/graphassembly.hpp
@@ -2,14 +2,45 @@
 #define GRAPHASSEMBLY_H
 #include "structures.hpp"
 
-
+/**
+ * @brief These functions update the graph
+ * 
+ */
 class GraphAssembly
 {
     
     public:
-        //methods
+        //functions
+        /**
+         * @brief Create device and block controls
+         * 
+         * If multiple USRPs are used, they are linked into a single RFNoc graph.
+         * 
+         * @param graphSettings 
+         * @param args Address(es) of USRP(s)
+         */
         static void buildGraph(GraphSettings& graphSettings, const std::string& args);
+
+
+        /**
+         * @brief Seek radio blocks on each USRP and assemble a vector of radio
+         *        controllers.
+         * 
+         * updates the graphSettings with
+         *      - Aquires all USRP blocks into -> radio_block_list
+         *      - Adds all radio_ctrls for the USRPs
+         * 
+         * and sorts both radio_block_list and _radio_ctrls.
+         * 
+         * @param graphSettings Sets the radio_block_list
+         */
         static void buildRadios(GraphSettings& graphSettings);
+
+        /**
+         * @brief 
+         * 
+         * @param graphSettings 
+         */
         static void buildDDCDUC(GraphSettings& graphSettings);
         static void buildReplay(GraphSettings& graphSettings); 
         static void buildStreams(GraphSettings& graphSettings, const std::string& streamargs, const std::string& format, const std::string& otw); 
