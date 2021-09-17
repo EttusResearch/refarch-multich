56e3bdc5c4d951e5cb828ae71684ff4132acaed3
jovian <jovian.wysocki@ni.com>
2021-09-17 14:01:18 -0500

    Updated Ethtool.

--- config/nicrb.sh
+++ config/nicrb.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#link_name=("enp225s0f0" "enp225s0f1" "enp225s0f2" "enp225s0f3" "enp193s0f0" "enp193s0f1" "enp193s0f2" "enp193s0f3" "enp1s0f0" "enp1s0f1" "enp1s0f2" "enp1s0f3" "enp37s0f0" "enp37s0f1" "enp37s0f2" "enp37s0f3")
-#for link in "${link_name[@]}"
-#do
-#    sudo ethtool -G $link tx 4096 rx 4096
-#done
+ethtool_setup(){
+    link_name=("ens4f0" "ens4f1" "ens4f2" "ens4f3" "ens6f0" "ens6f1" "ens6f2" "ens6f3" "ens9f0" "ens9f1" "ens9f2" "ens9f3" "ens10f0" "ens10f1" "ens10f2" "ens10f3")
+    for link in "${link_name[@]}"
+    do
+        sudo ethtool -G $link tx 4096 rx 4096
+    done
+}
 main(){
 ETH_CARDS=($(ip link | grep -E -o '[0-9]+: [0-9­A-Z­a-z]+:' | sed 's/^[0-9]*: //;s/://'))
 
 print_arry "${ETH_CARDS[@]}"
-
+ethtool_setup
 
 }
 
