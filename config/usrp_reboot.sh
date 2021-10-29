25867cdb01b3ffc21bc24f66feb587c5efb61f49
Dylan Baros <74726798+dylan-baros@users.noreply.github.com>
2021-10-29 10:11:15 -0500

    Update usrp_reboot.sh
    Removed Whitespace

--- config/usrp_reboot.sh
+++ config/usrp_reboot.sh
@@ -3,15 +3,11 @@
 #Reboot All USRPS
 main(){
     check_root
-
     rm usrp_config.txt
     echo "Determining USRP IP Addresses"
     uhd_find_devices > usrp_config.txt
     echo 'Information Stored in usrp_config.txt'
-    
     FILENAME="usrp_config.txt"
-   
-
     #loop over ip addresses
     myarr=($(awk '$1 ~ /addr:/ && $1 !~ /mgmt_addr/ {print $2}' usrp_config.txt))
 
@@ -21,18 +17,11 @@ main(){
     if user_input "$tempText"; then
       for i in "${myarr[@]}"
       do
-
-        echo "Rebooting USRP: "$i
-        
-        
+        echo "Rebooting USRP: "$i 
         ssh root@$i 'reboot'
-        
-
-    
         let COUNTER++
         done
     fi 
-  
     exit 0
 }
 
@@ -59,7 +48,5 @@ user_input(){
       esac
   done
 }
-
-
 
 main "$@"; exit
