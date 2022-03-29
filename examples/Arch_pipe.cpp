00be81bd7d706f6a2a6bd4023e8dc9470299b5b2
NI-jovian <jovian.wysocki@ni.com>
2022-03-29 15:20:04 -0500

    Fixed incorrect message on Pipe example

--- examples/Arch_pipe.cpp
+++ examples/Arch_pipe.cpp
@@ -330,7 +330,7 @@ public:
                 << (boost::format(
                         " Could not set buffer size to %d. Instead set to %d \n"
                         " Try setting permissions of this executable with this command\n "
-                        " sudo setcap 'CAP_SYS_RESOURCE=+ep' Arch_pipe_example\n "
+                        " sudo setcap 'CAP_SYS_RESOURCE=+ep' Arch_pipe\n "
                         " Or by reducing the pipe file buffer size inside the "
                         "configuration file. ")
                        % pipe_file_buffer_size % file_size_buffer);
