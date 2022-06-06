60a628e5777e2b1e869030db5cd881454cbde9a8
Dylan Baros <dylan.baros@ni.com>
2022-06-06 14:00:29 -0500

    Fixed time

--- lib/RefArch.cpp
+++ lib/RefArch.cpp
@@ -1399,7 +1399,7 @@ void RefArch::asyncTimer(int threadnum){
   std::cout << "Entering Thread" << std::flush;
   int time =0;
   while (RA_stop_signal_called == false and time <= RA_time_requested){
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     time++;
   };
   RA_stop_signal_called = true;
